a5f8b6afabd7d287c41fb78b7a48489066c4b89b
CharukaHS <Charuka12@gmail.com>
2018-09-13 21:35:27 +0530

    added infinite scroll

--- js/script.js
+++ js/script.js
@@ -1,17 +1,64 @@
+var reachedBtm = false;
+var allLoaded = false;
+var i = 0;
+var n = 15;
+var items = [];
+
 $(document).ready(function(){
   
-  $.getJSON("./json/artwork.json", function(data) {
-    var items = []
+  $.getJSON("./json/artwork.json", function(data) {    
     $.each(data, function (indexInArray, valueOfElement) { 
-       items.push(valueOfElement)       
+       items.push(valueOfElement);   
     });
-    console.log(items);
+    console.log('length =', items.length);
+    addItems()
   });
-  
-  
 
+  $(window).on('scroll', function() {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      if(!reachedBtm && !allLoaded) {
+        reachedBtm = true;
+        addItems();     
+      }
+    }
+  })
 
+  //DOM 
   $('span').click(function(){
     $('#nav').addClass('navbarClicked')
   })
 })
+
+function addItems() {  
+  if (items.length >= n) {
+    var looped = 0
+    for (; i < n; i++) {      
+      if (i === items.length) {
+        allLoaded = true;
+        console.log(i , 'break');      
+        break;
+      }
+      var item = items[i]
+      $(".showcase").append(`
+      <div class="artwork">
+        <img src="${item.thumbnail}" alt="">
+        <div class="description">
+          <span>
+            <h3>${item.title}</h3>
+            <p>${item.discription}</p>
+          </span>
+        </div>
+      </div>
+      `)
+      looped++
+    }
+    n += looped
+    if ( items.length < n) {
+      n = items.length
+    }
+    reachedBtm = false;
+  } else {
+    console.log('done');    
+    allLoaded = true;
+  }
+}
